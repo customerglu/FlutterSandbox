5f8542ef187fb7f18f6e9e049689588bbef1ee38
Himanshu Trehan <88142817+himCustomerGlu@users.noreply.github.com>
2024-06-12 13:55:46 +0530

    Current class name added

--- lib/page/splash_page.dart
+++ lib/page/splash_page.dart
@@ -7,91 +7,100 @@ import '../core/color.dart';
 import '../widgets/bottom_nav.dart';
 
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    CustomergluPlugin.setCurrentClassName("Splash");
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return CGScreenDetector(
-      classname: 'Splash',
-      child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            const SizedBox(height: 35),
-            const Text(
-              'Let\'s plant with us',
-              style: TextStyle(
-                fontSize: 22.0,
-                letterSpacing: 1.8,
-                fontWeight: FontWeight.w900,
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const SizedBox(height: 35),
+          const Text(
+            'Let\'s plant with us',
+            style: TextStyle(
+              fontSize: 22.0,
+              letterSpacing: 1.8,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          const SizedBox(height: 5),
+          const Text(
+            'Bring nature home',
+            style: TextStyle(
+              color: grey,
+              fontSize: 16,
+              letterSpacing: 1.8,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 450,
+            width: 450,
+            child: Image.asset('assets/images/Asset1.png'),
+          ),
+          const SizedBox(height: 25),
+          GestureDetector(
+            onTap: () {
+              registerUserWithCG(context);
+
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 80.0,
+                vertical: 12.0,
+              ),
+              decoration: BoxDecoration(
+                color: green,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: const Text(
+                'Sign In',
+                style: TextStyle(
+                  color: white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
-            const SizedBox(height: 5),
-            const Text(
-              'Bring nature home',
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Create an account',
               style: TextStyle(
-                color: grey,
+                color: black.withOpacity(0.7),
                 fontSize: 16,
-                letterSpacing: 1.8,
+                letterSpacing: 1,
                 fontWeight: FontWeight.w600,
               ),
             ),
-            SizedBox(
-              height: 450,
-              width: 450,
-              child: Image.asset('assets/images/Asset1.png'),
-            ),
-            const SizedBox(height: 25),
-            GestureDetector(
-              onTap: () {
-                registerUserWithCG(context);
-
-              },
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 80.0,
-                  vertical: 12.0,
-                ),
-                decoration: BoxDecoration(
-                  color: green,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: const Text(
-                  'Sign In',
-                  style: TextStyle(
-                    color: white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Forgot Password?',
+              style: TextStyle(
+                color: black.withOpacity(0.4),
+                letterSpacing: 1,
+                fontWeight: FontWeight.w600,
               ),
             ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                'Create an account',
-                style: TextStyle(
-                  color: black.withOpacity(0.7),
-                  fontSize: 16,
-                  letterSpacing: 1,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                'Forgot Password?',
-                style: TextStyle(
-                  color: black.withOpacity(0.4),
-                  letterSpacing: 1,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
